60198c418900316a279bf6a5aef9555239fbd1a6
Cole Fortner <colefortner@gmail.com>
2022-04-01 16:58:49 -0400

    Create forms to enter daily steps and weight.  These are duplicates, think if you will be andding fields to these.  If not, try to make this a reusable component.

--- src/fitness/steps/DailyStepsForm.tsx
+++ src/fitness/steps/DailyStepsForm.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DailyStepsForm: React.FC = () => {
+  const [steps, setSteps] = useState("");
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSteps(event.target.value);
+  };
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("Your steps today");
+    console.log(steps);
+    console.log(new Date());
+  };
   return (
     <>
-      <h1>Daily Weight Form</h1>
+      <h1>Daily Steps Form</h1>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="steps">Today's Steps</label>
+        <input
+          id="steps"
+          type="text"
+          placeholder="enter steps"
+          name="steps"
+          onChange={changeHandler}
+          value={steps}
+          required
+        />
+        <button type="submit">submit</button>
+      </form>
     </>
   );
 };
--- src/weight/DailyWeightForm.tsx
+++ src/weight/DailyWeightForm.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DailyWeightForm: React.FC = () => {
+  const [weight, setWeight] = useState("");
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWeight(event.target.value);
+  };
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("Your weight today");
+    console.log(weight);
+    console.log(new Date());
+  };
+
   return (
     <>
       <h1>Daily Weight Form</h1>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="weight">Today's Weight</label>
+        <input
+          id="weight"
+          type="text"
+          placeholder="enter weight"
+          name="weight"
+          onChange={changeHandler}
+          value={weight}
+          required
+        />
+        <button type="submit">submit</button>
+      </form>
     </>
   );
 };
